6cac7320d3abd27728208a1855b06d8791038fc1
Joshua Bradford <jjyet@yahoo.com>
2017-03-10 13:20:34 -0500

    worked on planning out the project in notes file

---
+++ server/notes.js
@@ -0,0 +1,17 @@
+ROUTES
+
+1. users sign up
+2. users sign in
+3. any user retrieve links from most recent to least recent
+4. Logged in users can submit links with at least a title and destination (url)
+5. Logged in users can add comments on any link.
+6. Any user can retrieve the comments for a given link.
+
+USERS TABLE
+id | username | email | userpassword(encrypted) | usersalt
+
+LINKS TABLE
+id | title | destination_url | user that posted it
+
+COMMENTS TABLE
+id | link id(foreign id) | user id that made comment |text
